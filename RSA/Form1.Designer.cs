befd2618b6911b688d010337a27a5f4ce1ada9b9
Alexey <alexbashur@gmail.com>
2018-10-07 09:59:55 +0300

    Добавьте файлы проекта.

---
+++ RSA/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace RSA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonEncr = new System.Windows.Forms.Button();
+            this.buttonDecr = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxMsg = new System.Windows.Forms.TextBox();
+            this.textBoxOpenE = new System.Windows.Forms.TextBox();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxOpenN = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBoxSecretN = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxSecretD = new System.Windows.Forms.TextBox();
+            this.buttonGenerateKeys = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonEncr
+            // 
+            this.buttonEncr.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonEncr.Location = new System.Drawing.Point(8, 180);
+            this.buttonEncr.Name = "buttonEncr";
+            this.buttonEncr.Size = new System.Drawing.Size(112, 23);
+            this.buttonEncr.TabIndex = 0;
+            this.buttonEncr.Text = "Зашифровать";
+            this.buttonEncr.UseVisualStyleBackColor = true;
+            this.buttonEncr.Click += new System.EventHandler(this.buttonEncr_Click);
+            // 
+            // buttonDecr
+            // 
+            this.buttonDecr.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDecr.Location = new System.Drawing.Point(126, 180);
+            this.buttonDecr.Name = "buttonDecr";
+            this.buttonDecr.Size = new System.Drawing.Size(119, 23);
+            this.buttonDecr.TabIndex = 1;
+            this.buttonDecr.Text = "Расшифровать";
+            this.buttonDecr.UseVisualStyleBackColor = true;
+            this.buttonDecr.Click += new System.EventHandler(this.buttonDecr_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Сообщение:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(6, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "N:";
+            // 
+            // textBoxMsg
+            // 
+            this.textBoxMsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxMsg.Location = new System.Drawing.Point(103, 6);
+            this.textBoxMsg.Name = "textBoxMsg";
+            this.textBoxMsg.Size = new System.Drawing.Size(432, 22);
+            this.textBoxMsg.TabIndex = 5;
+            this.textBoxMsg.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxMsg_KeyPress);
+            // 
+            // textBoxOpenE
+            // 
+            this.textBoxOpenE.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxOpenE.Location = new System.Drawing.Point(32, 24);
+            this.textBoxOpenE.Name = "textBoxOpenE";
+            this.textBoxOpenE.Size = new System.Drawing.Size(219, 22);
+            this.textBoxOpenE.TabIndex = 6;
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxResult.Location = new System.Drawing.Point(69, 152);
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(460, 22);
+            this.textBoxResult.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(5, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Лог:";
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxLog.Location = new System.Drawing.Point(45, 209);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxLog.Size = new System.Drawing.Size(490, 170);
+            this.textBoxLog.TabIndex = 9;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxOpenN);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBoxOpenE);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(15, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(257, 100);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Открытый ключ:";
+            // 
+            // textBoxOpenN
+            // 
+            this.textBoxOpenN.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxOpenN.Location = new System.Drawing.Point(32, 59);
+            this.textBoxOpenN.Name = "textBoxOpenN";
+            this.textBoxOpenN.Size = new System.Drawing.Size(219, 22);
+            this.textBoxOpenN.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(6, 27);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "E:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(12, 155);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 16);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Ответ:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBoxSecretN);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.textBoxSecretD);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox2.Location = new System.Drawing.Point(278, 34);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(257, 100);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Секретный ключ:";
+            // 
+            // textBoxSecretN
+            // 
+            this.textBoxSecretN.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxSecretN.Location = new System.Drawing.Point(32, 59);
+            this.textBoxSecretN.Name = "textBoxSecretN";
+            this.textBoxSecretN.Size = new System.Drawing.Size(219, 22);
+            this.textBoxSecretN.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(6, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "D:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(6, 62);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(21, 16);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "N:";
+            // 
+            // textBoxSecretD
+            // 
+            this.textBoxSecretD.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxSecretD.Location = new System.Drawing.Point(32, 24);
+            this.textBoxSecretD.Name = "textBoxSecretD";
+            this.textBoxSecretD.Size = new System.Drawing.Size(219, 22);
+            this.textBoxSecretD.TabIndex = 6;
+            // 
+            // buttonGenerateKeys
+            // 
+            this.buttonGenerateKeys.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonGenerateKeys.Location = new System.Drawing.Point(251, 180);
+            this.buttonGenerateKeys.Name = "buttonGenerateKeys";
+            this.buttonGenerateKeys.Size = new System.Drawing.Size(127, 23);
+            this.buttonGenerateKeys.TabIndex = 12;
+            this.buttonGenerateKeys.Text = "Сгенерировать ключи";
+            this.buttonGenerateKeys.UseVisualStyleBackColor = true;
+            this.buttonGenerateKeys.Click += new System.EventHandler(this.buttonGenerateKeys_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 391);
+            this.Controls.Add(this.buttonGenerateKeys);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.textBoxMsg);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonDecr);
+            this.Controls.Add(this.buttonEncr);
+            this.Name = "Form1";
+            this.Text = "RSA";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonEncr;
+        private System.Windows.Forms.Button buttonDecr;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxMsg;
+        private System.Windows.Forms.TextBox textBoxOpenE;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxOpenN;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBoxSecretN;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxSecretD;
+        private System.Windows.Forms.Button buttonGenerateKeys;
+    }
+}
+
---
+++ RSA/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace RSA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            RSAEncryptDecrypt.Log = textBoxLog;
+        }
+
+        private void buttonGenerateKeys_Click(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+
+            textBoxLog.Clear();
+
+            RSAEncryptDecrypt.GenerateKeys(out int N, out int E, out long D);
+
+            textBoxOpenE.Text = Convert.ToString(E);
+            textBoxOpenN.Text = Convert.ToString(N);
+            textBoxSecretN.Text = Convert.ToString(N);
+            textBoxSecretD.Text = Convert.ToString(D);
+
+            this.Cursor = Cursors.Default;
+        }
+
+        private void textBoxMsg_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!RSAEncryptDecrypt.alfavit.Contains(e.KeyChar) && e.KeyChar != 8 && e.KeyChar != ' ')
+                e.Handled = true;
+        }
+
+        private void buttonEncr_Click(object sender, EventArgs e)
+        {
+            if (textBoxOpenE.Text == "" || textBoxOpenN.Text == "")
+                MessageBox.Show("Открытый ключ не введён!");
+            else
+                if (textBoxMsg.Text == "")
+                    MessageBox.Show("Сообщение не введено!");
+                else
+                {
+                    textBoxResult.Text = RSAEncryptDecrypt.Encrypt(Convert.ToInt32(textBoxOpenE.Text), Convert.ToInt32(textBoxOpenN.Text), textBoxMsg.Text);
+                }
+        }
+
+        private void buttonDecr_Click(object sender, EventArgs e)
+        {
+            if (textBoxSecretD.Text == "" || textBoxSecretN.Text == "")
+                MessageBox.Show("Закрытый ключ не введён!");
+            else
+                if (textBoxMsg.Text == "")
+                MessageBox.Show("Сообщение не введено!");
+            else
+            {
+                textBoxResult.Text = RSAEncryptDecrypt.Decrypt(Convert.ToInt32(textBoxSecretD.Text), Convert.ToInt32(textBoxSecretN.Text), textBoxMsg.Text);
+            }
+        }
+    }
+
+    static class RSAEncryptDecrypt
+    {
+        //--------------------------------
+        //   Константы
+        //--------------------------------
+        public static readonly string alfavit = "||абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123456789.,:;!() abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        //--------------------------------
+        //   Лог
+        //--------------------------------
+        public static TextBox Log;
+
+        //--------------------------------
+        //   Генерация простых чисел
+        //--------------------------------
+        private static List<int> ReshetoEratosfena(out int P, out int Q)
+        {
+            Log.AppendText("--------------------------" + Environment.NewLine);
+            Log.AppendText("Генерация простых чисел"+Environment.NewLine);
+            Log.AppendText("--------------------------" + Environment.NewLine);
+
+            var IntArr = new List<int>();
+            for (var i = 2; i <= 20000; i++)
+                IntArr.Add(i);
+
+            for (var i = 0; i < IntArr.Count; i++)
+            {
+                for (var j = i; j < IntArr.Count; j++)
+                {
+                    if ((IntArr[i] != IntArr[j]) && (IntArr[j] % IntArr[i] == 0))
+                    {
+                        IntArr.RemoveAt(j);
+                        j--;
+                    }
+                }
+            }
+
+            var ResList = new List<int>(IntArr);
+
+            for (var i = 0; i < IntArr.Count;)
+                if (IntArr[i] < 10000)
+                    IntArr.Remove(IntArr[i]);
+                else
+                    break;
+            Log.AppendText("Всего простых от 10000 до 20000: "+Convert.ToString(IntArr.Count) + Environment.NewLine);
+
+            P = IntArr[new Random().Next(0, IntArr.Count - 1)];
+            do
+            {
+                Q = IntArr[new Random().Next(0, IntArr.Count - 1)];
+            }
+            while (Q == P);
+
+            Log.AppendText("Случайное число P из диапазона: " + Convert.ToString(P) + Environment.NewLine);
+            Log.AppendText("Случайное число Q из диапазона: " + Convert.ToString(Q) + Environment.NewLine);
+
+            Log.AppendText("--------------------------" + Environment.NewLine);
+
+            return ResList;
+        }
+        
+        //--------------------------------
+        //   Алгоритм Евклида
+        //--------------------------------
+        private static bool EuclidIsNOD1(int a, int b)
+        {
+
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a -= b;
+                else
+                    b -= a;
+            }
+            if (a == 1 || b == 1)
+                return false;
+            else
+                return true;
+        }
+
+        //--------------------------------
+        //   Генерация ключей
+        //--------------------------------
+        public static void GenerateKeys(out int N, out int E, out long D)
+        {
+            Log.AppendText("--------------------------" + Environment.NewLine);
+            Log.AppendText("Генерация ключа" + Environment.NewLine);
+            Log.AppendText("--------------------------" + Environment.NewLine);
+
+            var P = 0;
+            var Q = 0;
+            var ProstList = RSAEncryptDecrypt.ReshetoEratosfena(out P, out Q);
+
+            N = P * Q;
+            Log.AppendText("N = "+ Convert.ToString(N) + Environment.NewLine);
+
+            var M = (P - 1) * (Q - 1);
+            Log.AppendText("M = "+ Convert.ToString(M) + Environment.NewLine);
+
+            E = 0;
+            while (true)
+            {
+                var rnd = ProstList[new Random().Next(0, ProstList.Count - 1)];
+                if (!EuclidIsNOD1(rnd, M))
+                {
+                    E = rnd;
+                    break;
+                }
+            }
+            Log.AppendText("E = " + Convert.ToString(E) + Environment.NewLine);
+
+            D = 1;
+            while (D * E % M != 1)
+            {
+                D++;
+            }
+            Log.AppendText("D = " + Convert.ToString(D) + Environment.NewLine);
+        }
+
+        //--------------------------------
+        //   Шифрование
+        //--------------------------------
+        public static string Encrypt(int E, int N, string msg)
+        {
+            Log.AppendText("--------------------------" + Environment.NewLine);
+            Log.AppendText("Шифрование" + Environment.NewLine);
+            Log.AppendText("--------------------------" + Environment.NewLine);
+            Log.AppendText("Исходная строка: "+ msg + Environment.NewLine);
+
+            var ResString = "";
+            var tmpN = new BigInteger(N);
+            var tmpE = new BigInteger(E);
+
+            for (var i = 0; i < msg.Length; i++)
+            {
+                var tmpIndex = new BigInteger(alfavit.IndexOf(msg[i]));
+                var Rem = BigInteger.ModPow(tmpIndex, tmpE, tmpN);
+                if (i != 0)
+                    ResString += " ";
+                ResString += Rem.ToString();
+            }
+
+            Log.AppendText("Ответ: " + ResString + Environment.NewLine);
+            return ResString;
+        }
+
+        //--------------------------------
+        //   Расшифровка
+        //--------------------------------
+        public static string Decrypt(int D, int N, string msg)
+        {
+            Log.AppendText("--------------------------" + Environment.NewLine);
+            Log.AppendText("Расшифровка" + Environment.NewLine);
+            Log.AppendText("--------------------------" + Environment.NewLine);
+            Log.AppendText("Исходная строка: " + msg + Environment.NewLine);
+
+            var tmpN = new BigInteger(N);
+            var tmpD = new BigInteger(D);
+
+            var msgsplit = msg.Split(' ');
+
+            var ResString = "";
+            for (var i = 0; i < msgsplit.Length; i++)
+            {
+                var Rem = BigInteger.ModPow(new BigInteger(Convert.ToInt32(msgsplit[i])), tmpD, tmpN);
+                ResString += alfavit[Convert.ToInt32(Rem.ToString())];
+            }
+
+            Log.AppendText("Ответ: " + ResString + Environment.NewLine);
+            return ResString;
+        }
+    }
+}
